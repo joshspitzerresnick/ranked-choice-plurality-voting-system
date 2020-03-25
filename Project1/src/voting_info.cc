df504a446c7d5b07b4d34f98b701bc3d50c9fcaa
Josh Spitzer-Resnick <spitz123@umn.edu>
2020-03-25 01:16:53 -0500

    add(src/voting_info.cc)

---
+++ Project1/src/voting_info.cc
@@ -0,0 +1,73 @@
+/**
+ * @file voting_info.cc
+ *
+ * @copyright 2020 Josh Spitzer-Resnick, spitz123
+ */
+
+#include <iostream>
+#include <string>
+#include <list>
+
+#include "src/voting_info.h"
+
+VotingInfo::VotingInfo(int algorithm, int seats) {
+  algorithm_ = algorithm;
+  num_seats_ = seats;
+  num_candidates_ = 0;
+  candidate_list_ = NULL;
+  num_ballots_ = 0;
+  ballot_list_ = NULL;
+}
+
+VotingInfo::~VotingInfo() {
+  for (int i = 0; i < candidate_list_.size(); i++) {
+    candidate_list_[i] = NULL;
+  }
+  for (int i = 0; i < ballot_list_.size(); i++) {
+    ballot_list_[i] = NULL;
+  }
+  candidate_list_.clear();
+  ballot_list_.clear();
+}
+
+int VotingInfo::GetAlgorithm() const {
+  return algorithm_;
+}
+
+int VotingInfo::GetNumSeats() const {
+  return num_seats_;
+}
+
+void VotingInfo::AddCandidateToCandidateList(Candidate* candidate) {
+  candidate_list_.push_back(candidate);
+  IncrementNumCandidates();
+}
+
+void VotingInfo::AddBallotToBallotList(Ballot* ballot) {
+  ballot_list_.push_back(ballot);
+  IncrementNumBallots();
+}
+
+int VotingInfo::GetNumCandidates() const {
+  return num_candidates_;
+}
+
+int VotingInfo::GetNumBallots() const {
+  return num_ballots_;
+}
+
+std::list<Candidate*> VotingInfo::GetCandidateList() const {
+  return candidate_list_;
+}
+
+std::list<Ballot*> VotingInfo::GetBallotList() const {
+  return ballot_list_;
+}
+
+void VotingInfo::IncrementNumCandidates() {
+  num_candidates_++;
+}
+
+void VotingInfo::IncrementNumBallots() {
+  num_ballots_++;
+}
