b0a543049bc09c4336410b822edab2fb87aa1f91
deak0007 <deak0007@umn.edu>
2020-04-01 08:41:41 -0500

    Added doxygen friendly comments, fix bugs

--- Project1/src/stv_election.cc
+++ Project1/src/stv_election.cc
@@ -4,32 +4,38 @@
 #include <cmath>
 
 explicit STVElection::STVElection(VotingInfo* votingInfo) {
-    numSeats_ = votingInfo->GetNumSeats();
-    int numBallots = votingInfo->GetNumBallots();
-    int droop = (int)(floor(((double)numBallots)/((double)numSeats_+1))+1);
-    stvElectionRecord_ = new stvElectionRecord_(votingInfo.GetCandidateList(), votingInfo.GetBallotList(), droop);
-    stvResultDisplay_= new ResultDisplay();
+  numSeats_ = votingInfo->GetNumSeats();
+  int numBallots = votingInfo->GetNumBallots();
+  int droop = (int)(floor(((double)numBallots)/((double)numSeats_+1))+1);
+  stvElectionRecord_ = new stvElectionRecord_(votingInfo.GetCandidateList(), votingInfo.GetBallotList(), droop);
+  stvResultDisplay_= new ResultDisplay();
 }
 
 STVElection::RunElection(){
-    STVCandidate* candidate;
-    std::list<Ballot*> ballotList;
-    // check if ballot shuffle off option is true
-    if (~BallotShuffleOff){
-        stvElectionRecord_->ShuffleBallots();
+  STVCandidate* candidate; // stv candidate object pointer to hold candidate object to pass between member functions
+  std::list<Ballot*> ballotList; // ballot pointer list to hold ballots for passing between stvelectionrecord functions
+  // check if ballot shuffle off option is true
+  if (~BallotShuffleOff){
+    stvElectionRecord_->ShuffleBallots(); // shuffle ballots
+  }
+  while (true){
+    stvElectionRecord_->DistributeBallots(); // distribute ballots
+    // when there is no more candidate on nonelected list, exit loop
+    if (stvElectionRecord_->nonElectedCandidateList_.empty()){
+      break;
     }
-    while (true){        
-        stvElectionRecord_->DistributeBallots();
-        if (stvElectionRecord_->nonElectedCandidateList_.empty()){
-            break;
-        }
-        candidate = stvElectionRecord_->RemoveLastCandidateFromNonElectedCandidateList();
-        ballotList = stvElectionRecord_->AddCandidateToLosersList(candidate);
-        stvElectionRecord_->AddLoserBallotsToNonDistributedBallotList(ballotList);
-    }
-    while ((int)(stvElectionRecord_->winnersList_.size()) < numSeats_){
-        candidate = stvElectionRecord_->PopCandidateOffLosersList();
-        stvElectionRecord_->AddCandidateToWinnersList(candidate);
-    }
-    stvResultDisplay_->DisplayResult(stvElectionRecord_);
+    // Sort non-elected candidate list by number of votes, break tie (embeded) if number of votes are equal
+    stvElectionRecord_->SortNonElectedCandidateList(); 
+    // Put the candidate with the least votes onto losers list and put his/her ballots into non-distributed balots list
+    candidate = stvElectionRecord_->RemoveLastCandidateFromNonElectedCandidateList();
+    ballotList = stvElectionRecord_->AddCandidateToLosersList(candidate);
+    stvElectionRecord_->AddLoserBallotsToNonDistributedBallotList(ballotList);
+  }
+  // if need more candidates to fill seats, move the candidates being put on losers list last to winners list
+  while ((int)(stvElectionRecord_->winnersList_.size()) < numSeats_){
+    candidate = stvElectionRecord_->PopCandidateOffLosersList();
+    stvElectionRecord_->AddCandidateToWinnersList(candidate);
+  }
+  // display election results
+  stvResultDisplay_->DisplayResult(stvElectionRecord_);
 }
--- Project1/src/stv_election.h
+++ Project1/src/stv_election.h
@@ -1,16 +1,42 @@
+/**
+ * @file stv_election.h
+ *
+ * @copyright 2020 5801 Team3, All rights reserved.
+ */
+
+#ifndef SRC_STV_ELECTION_H_
+#define SRC_STV_ELECTION_H_
 
 #include "voting_info.h"
 #include "stv_election_record.h"
 #include "candidate.h"
 #include "ballot.h"
 
-
+/**
+ * @brief The main class for stv election.
+ *
+ * Is an stvelection.
+ */
 class STVElection{
-    private:
-    STVElectionRecord* stvElectionRecord_;
-    STVResultDisplay* stvResultDisplay_;
-    int numSeats_;
-    public:
-    STVElection(VotingInfo*);
-    void RunElection();    
+ public:
+  /**
+  * @brief Constructs an stvelection object to be used in an election.
+  *
+  * @param[in] VotingInfo* data structure
+  *
+  */
+  STVElection(VotingInfo*);
+  /**
+  * @brief Run an STV election.
+  *
+  * @param[in] none
+  * @return void
+  *
+  */
+  void RunElection();
+ private: 
+  STVElectionRecord* stvElectionRecord_;  // stv election record object
+  STVResultDisplay* stvResultDisplay_;  // stv result display object
+  int numSeats_;  // number of seats to be filled
 }
+#endif
