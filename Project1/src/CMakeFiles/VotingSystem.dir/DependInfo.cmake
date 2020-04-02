6e1805ae4bc3b15698f2878d0445059f9ed8e668
Bryan John Baker <bake1358@umn.edu>
2020-04-02 16:17:00 -0500

    Added cmake files

---
+++ Project1/src/CMakeFiles/VotingSystem.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/ballot.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/ballot.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/ballot_file_processor.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/ballot_file_processor.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/candidate.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/candidate.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/logger.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/logger.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/plurality_election.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/plurality_election.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/plurality_election_record.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/plurality_election_record.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/stv_election.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/stv_election.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/stv_election_record.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/stv_election_record.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/voting_info.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/voting_info.cc.o"
+  "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/voting_system.cc" "/home/bbaker/Documents/CSCI5801/repo-Team3/Project1/src/CMakeFiles/VotingSystem.dir/voting_system.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
