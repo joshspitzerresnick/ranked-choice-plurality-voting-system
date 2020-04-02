922310d7bd897bfc07f597ee571d454c0abbfe6c
Bryan John Baker <bake1358@umn.edu>
2020-04-02 18:33:20 -0500

    cpplint compliance for plurality election record.cc

--- Project1/src/plurality_election_record.cc
+++ Project1/src/plurality_election_record.cc
@@ -7,117 +7,90 @@
 #include <stdlib.h>
 #include "plurality_election_record.h"
 
-PluralityElectionRecord::PluralityElectionRecord(std::list<Candidate*> candidates, std::list<Ballot*> ballots)
-{
+PluralityElectionRecord::
+PluralityElectionRecord(std::list<Candidate*> candidates,
+                                                 std::list<Ballot*> ballots) {
     this->nonDistributedBallotList_ = ballots;
     this->nonElectedCandidateList_ = candidates;
     this->winnersList_ = {};
     this->losersList_ = {};
 }
 
-void PluralityElectionRecord::DistributeBallots()
-{
-
+void PluralityElectionRecord::DistributeBallots() {
     Ballot* curr_ballot;
     int curr_candidate_id;
-    while(!nonDistributedBallotList_.empty())
-    {
+    while (!nonDistributedBallotList_.empty()) {
         curr_ballot = nonDistributedBallotList_.front();
         curr_candidate_id = curr_ballot->GetRankedCandidateIDList().front();
         std::list<Candidate*>::iterator it;
-        for(it = this->nonElectedCandidateList_.begin(); it != this->nonElectedCandidateList_.end(); it++)
-        {
-            if((*it)->GetID() == curr_candidate_id)
-            {
+        for (it = this->nonElectedCandidateList_.begin();
+             it != this->nonElectedCandidateList_.end(); it++) {
+            if ((*it)->GetID() == curr_candidate_id) {
                 (*it)->AddBallot(curr_ballot);
                 break;
             }
         }
         nonDistributedBallotList_.pop_front();
-
-
-
     }
 }
 
-void PluralityElectionRecord::SortNonElectedCandidateList()
-{
+void PluralityElectionRecord::SortNonElectedCandidateList() {
     nonElectedCandidateList_.sort(CandidateNumBallotsComp);
 }
 
 
-bool PluralityElectionRecord::BreakTies()
-{
+bool PluralityElectionRecord::BreakTies() {
     int rand_num = rand() % 2;
-    if(rand_num == 0)
-    {
+    if (rand_num == 0) {
         return true;
-    }
-    else
-    {
+    } else {
         return false;
     }
-    
-    
 }
 
-void PluralityElectionRecord::MoveFirstNCandidatesFromNonELectedListToWinnersList(int N)
-{
-    if(N > this->nonElectedCandidateList_.size())
-    {
+void PluralityElectionRecord::
+MoveFirstNCandidatesFromNonELectedListToWinnersList(int N) {
+    if (N > this->nonElectedCandidateList_.size()) {
         throw 1;
-    }   
-    for(int i = 0; i < N; i++)
-    {
+    }
+    for (int i = 0; i < N; i++) {
         Candidate* temp_candidate = this->nonElectedCandidateList_.front();
         this->winnersList_.push_back(temp_candidate);
         this->nonElectedCandidateList_.pop_front();
     }
 }
 
-void PluralityElectionRecord::MoveRemainingCandidatesToLosersList()
-{
-    while(!this->nonElectedCandidateList_.empty())
-    {
+void PluralityElectionRecord::MoveRemainingCandidatesToLosersList() {
+    while (!this->nonElectedCandidateList_.empty()) {
         Candidate* temp_candidate = this->nonElectedCandidateList_.front();
         this->losersList_.push_back(temp_candidate);
         this->nonElectedCandidateList_.pop_front();
     }
 }
 
-std::list<Candidate*> PluralityElectionRecord::GetNonElectedCandidateList()
-{
+std::list<Candidate*> PluralityElectionRecord::GetNonElectedCandidateList() {
     return this->nonElectedCandidateList_;
 }
 
-std::list<Candidate*> PluralityElectionRecord::GetWinnersList()
-{
+std::list<Candidate*> PluralityElectionRecord::GetWinnersList() {
     return this->winnersList_;
 }
 
 
-std::list<Candidate*> PluralityElectionRecord::GetLosersList()
-{
+std::list<Candidate*> PluralityElectionRecord::GetLosersList() {
     return this->losersList_;
 }
 
 
-std::list<Ballot*> PluralityElectionRecord::GetNonDistributedBallotList()
-{
+std::list<Ballot*> PluralityElectionRecord::GetNonDistributedBallotList() {
     return this->nonDistributedBallotList_;
 }
 
-bool PluralityElectionRecord::CandidateNumBallotsComp(Candidate* candidate1, Candidate* candidate2)
-{
-    if(candidate1->GetNumBallots()  == candidate2->GetNumBallots())
-    {
-         return BreakTies();
-    }
-    else
-    {
+bool PluralityElectionRecord::CandidateNumBallotsComp(Candidate* candidate1,
+                                                      Candidate* candidate2) {
+    if (candidate1->GetNumBallots() == candidate2->GetNumBallots()) {
+        return BreakTies();
+    } else {
         return candidate1->GetNumBallots() > candidate2->GetNumBallots();
     }
-    
-
 }
-
