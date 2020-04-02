ea4805ee7fe3238c02e247bf2950892e1a47e9f8
Josh Spitzer-Resnick <spitz123@umn.edu>
2020-04-02 05:11:26 -0500

    fix(src/stv_election.cc): added std:: to endl

--- Project1/src/stv_election.cc
+++ Project1/src/stv_election.cc
@@ -1,4 +1,8 @@
-
+/**
+ * @file stv_election.cc
+ *
+ * @copyright 2020 5801 Team3, All rights reserved.
+ */
 
 #include "stv_election.h"
 #include <cmath>
@@ -26,7 +30,7 @@ STVElection::RunElection(){
       break;
     }
     // Sort non-elected candidate list by number of votes, break tie (embeded) if number of votes are equal
-    stvElectionRecord_->SortNonElectedCandidateList(); 
+    stvElectionRecord_->SortNonElectedCandidateList();
     // Put the candidate with the least votes onto losers list and put his/her ballots into non-distributed balots list
     candidate = stvElectionRecord_->RemoveLastCandidateFromNonElectedCandidateList();
     ballotList = stvElectionRecord_->AddCandidateToLosersList(candidate);
@@ -50,21 +54,21 @@ void STVElection::DisplayResult(){
   winnersList = stvElectionRecord_->GetWinnersList();
   losersList = stvElectionRecord_->GetLosersList();
   numCandidates = (int)winnersList.size() + (int)losersList.size();
-  std::cout << "---------------Election Result-----------------" << endl;
-  std::cout << "* Election Type: STV" << endl;
-  std::cout << "* #Seats: " << numSeats_ << endl;
-  std::cout << "* #Candidates: " << numCandidates << endl;
-  std::cout << "* Winners are: " << endl;
+  std::cout << "---------------Election Result-----------------" << std::endl;
+  std::cout << "* Election Type: STV" << std::endl;
+  std::cout << "* #Seats: " << numSeats_ << std::endl;
+  std::cout << "* #Candidates: " << numCandidates << std::endl;
+  std::cout << "* Winners are: " << std::endl;
   for (it = winnersList.begin(); it != winnersList.end(); it++)
   {
     std::advance(it, 1);
-    cout << ++orderNum << ": " << (*it)->GetName() << endl;    
+    cout << ++orderNum << ": " << (*it)->GetName() << std::endl;
   }
   orderNum = 0;
   for (it = LosersList.begin(); it != LosersList.end(); it++)
   {
     std::advance(it, 1);
-    cout << ++orderNum << ": " << (*it)->GetName() << endl;    
+    cout << ++orderNum << ": " << (*it)->GetName() << std::endl;
   }
-  cout << "-------------End of Result Display-------------" << endl;
+  cout << "-------------End of Result Display-------------" << std::endl;
 }
