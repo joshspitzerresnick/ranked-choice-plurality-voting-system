c692b2eb788d7e87c9f9303c8d4f1e2f211b716b
Josh Spitzer-Resnick <spitz123@umn.edu>
2020-04-02 14:53:27 -0500

    fix(src/stv_election_record.cc): comparator finally works

--- Project1/src/stv_election_record.cc
+++ Project1/src/stv_election_record.cc
@@ -137,9 +137,9 @@ struct STVCandidateComparator
 	// Compare 2 STVCandidate objects using number of ballots and order received
 	bool operator ()(const STVCandidate* candidate1, const STVCandidate* candidate2)
 	{
-		if (candidate1.GetNumBallots() == candidate2.GetNumBallots())
-        {return candidate1.GetFirstBallotNum()>candidate2.GetFirstBallotNum(); }
-      return candidate1.GetNumBallots() > candidate2.GetNumBallots(); 
+		if (candidate1->GetNumBallots() == candidate2->GetNumBallots())
+        {return candidate1->GetFirstBallotNum()>candidate2->GetFirstBallotNum(); }
+      return candidate1->GetNumBallots() > candidate2->GetNumBallots();
 	}
 };
 
